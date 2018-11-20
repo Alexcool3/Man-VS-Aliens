6904b10edac7cd0252ae78d29fd5fbb9573664eb
Alexcool3 <36537085+Alexcool3@users.noreply.github.com>
2018-11-20 22:36:33 +0100

    Updated PlayMove Script
    - Enable character to move linearly on a slope.
    - Experiment with slopeForce and SlopeForceRayLength to decide whether the player should move on a slope or fall down when the slope gets too steep.

--- Man VS Aliens/Assets/Man VS Aliens/Scripts/Player Movement/PlayerMove.cs
+++ Man VS Aliens/Assets/Man VS Aliens/Scripts/Player Movement/PlayerMove.cs
@@ -8,6 +8,9 @@ public class PlayerMove : MonoBehaviour {
     [SerializeField] private string verticalInputName;
     [SerializeField] private float movementSpeed;
 
+    [SerializeField] private float slopeForce;
+    [SerializeField] private float slopeForceRayLength;
+
     private CharacterController charController;
 
     [SerializeField] private AnimationCurve jumpFallOff;
@@ -28,15 +31,36 @@ public class PlayerMove : MonoBehaviour {
 
     private void PlayerMovement()
     {
-        float verInput = Input.GetAxis(verticalInputName) * movementSpeed;
-        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float verInput = Input.GetAxis(verticalInputName);
+        float horizInput = Input.GetAxis(horizontalInputName);
 
         Vector3 forwardMovement = transform.forward * verInput;
         Vector3 rightMovement = transform.right * horizInput;
 
-        charController.SimpleMove(forwardMovement + rightMovement);
+        charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+
+        if ((verInput != 0 || horizInput != 0) && OnSlope()) charController.Move(Vector3.down * charController.height / 2 * slopeForce * Time.deltaTime);
+       
+
         JumpInput();
 
+    }
+
+    private bool OnSlope()
+    {
+        if (isJumping) return false;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, charController.height/2 * slopeForceRayLength))
+        {
+            if(hit.normal != Vector3.up)
+            {
+                return true;
+            }
+           
+        }
+        return false;
     }
 
     private void JumpInput()
